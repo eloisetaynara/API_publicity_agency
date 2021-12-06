440fdeef2d9cd9b9aeccb79a2f64ad3fc89e1b1f
Eloise Taynara da Silva <eloisetds@hotmail.com>
2021-12-06 07:58:53 -0300

    Adicionando documentação

---
+++ README.md
@@ -0,0 +1,54 @@
+# Projeto Final de módulo 04 com o tema base Agência de publicidade
+
+## Desenvolvimento de API Rest em ambiente NodeJS utilizando framework framework web
+
+## Descrição
+API construida como como objeto de estudo, usando banco de dados não relacional SQLite3, seguindo o padrão de organização MVC, com possibilidade de aplicações Web, como softwares e sites, consistindo em um protocolo sobre a Arquitetura da Informação.
+
+## Ferramentas utilizadas
+JavaScript
+NodeJS 16.13.1 LTS
+Framework Express
+SQLite
+
+## OBS para rodar a API
+Basta dar o comando <b>npm start <b> ou <b>npm run dev<b>
+
+## Todas a dependências utilizadas
+```
+"dependencies": {
+    "dotenv": "^10.0.0",
+    "express": "^4.17.1",
+    "nodemon": "^1.10.2",
+    "sqlite": "^4.0.23",
+    "sqlite3": "^5.0.2"
+  }
+  ```
+  Este trecho pode ser encontrado dentro do arquivo <b>packge.json<b>
+
+## verbos HTTP e CRUD
+Os <b>verbos HTTP<b> vão indicar qual ação está sendo requisitada pelo consumidor do serviço.
+
+<b>CRUD<b>
+São as iniciais de das palavras em inglês Create, Read, Update, and Delete.
+
+## Exemplo de aplicação {
+    "id": 2,
+    "client": "Roy Mustang",
+    "salesman": "Lucas Teles",
+    "campaign": "Fullmetal Alchemist",
+    "budget": 1000,
+    "total": 1500,
+    "date": "16/10/2021",
+    "form_of_payment": "Crédito"
+  }
+## Rotas de vizualização
+GET + id: vizualiza os dados de um cadastro especifico dequele determinado número de id.
+
+GET + Sales: Apresenta todos os cadastro efetuados.
+
+POST + Sales: Inseri uma nova linha de dados.
+
+UPDATE + id: Atualizada o cadastro do id especificado.
+
+DELETE + id: Deleta o dado do id em especificado.
--- src/DAO/Sales.js
+++ src/DAO/Sales.js
@@ -1,39 +1,40 @@
 import { openDb } from "../config/configDB.js";
 
 // criando tabela
-let salesinho = await openDb();
-await salesinho.run('CREATE TABLE IF NOT EXISTS Sales (id INTEGER PRIMARY KEY NOT NULL, client VARCHAR(45), salesman VARCHAR(45), campaign VARCHAR(55), budget NUMBER(4,2), total NUMBER(4,2), date DATE, form_of_payment VARCHAR(45))')
+let db = await openDb();
+await db.run('CREATE TABLE IF NOT EXISTS Sales (id INTEGER PRIMARY KEY NOT NULL, client VARCHAR(45), salesman VARCHAR(45), campaign VARCHAR(55), budget NUMBER(4,2), total NUMBER(4,2), date DATE, form_of_payment VARCHAR(45))')
 
 //selecionando para poder inserir um dado depois
-export async function selectSales(req) { 
+export async function selectSales(req) {
   let id = req.params.id
-  return salesinho.get('SELECT * FROM sales WHERE id=?', [id])
+  return db.get('SELECT * FROM Sales WHERE id=?', [id])
 };
 
-// Inserindo uma nova tabela
+// Inserindo uma nova linha
 //inserindo um novo dado
-export async function insertSales() {
-  let Sales2 = req.body
-  salesinho.run('INSERT INTO Sales(client, salesman, campaign, budget, total, date, form_of_payment) VALUES (?,?,?,?,?,?,?)', [Sales2.client, Sales2.salesman, Sales2.campaign, Sales2.budget, Sales2.total, Sales2.date, Sales2.form_of_payment])
+export async function insertSales(sales2) {
+  openDb().then(db => {
+    db.run('INSERT INTO Sales(client, salesman, campaign, budget, total, date, form_of_payment) VALUES (?,?,?,?,?,?,?)', [sales2.client, sales2.salesman, sales2.campaign, sales2.budget, sales2.total, sales2.date, sales2.form_of_payment]);
+  })
 };
 
 //Atualização de uma tabela existente
 export async function selectSales2() {
-  return salesinho.all('SELECT * FROM Sales')
+  return db.all('SELECT * FROM Sales')
 };
 
 // adiciona dados para uma tabela existente
-export async function updadeSales(req) {
-  let Sales2 = req.body
-  let id = req.params.id
-  return salesinho.run(`UPDATE Sales2 SET client=?, salesman=?, campaign=?, budget=? , total=?, date=?, form_of_payment=? WHERE id=${id}`, [Sales2.client, Sales2.salesman, Sales2.campaign, Sales2.budget, Sales2.total, Sales2.date, Sales2.form_of_payment])
-};
+export async function updadeSales(sales2) {
+  openDb().then(db => {
+    return db.run(`UPDATE Sales SET client=?, salesman=?, campaign=?, budget=? , total=?, date=?, form_of_payment=? WHERE id=${id}`, [sales2.client, sales2.salesman, sales2.campaign, sales2.budget, sales2.total, sales2.date, sales2.form_of_payment])
+  });
+}
 
 //Deletando um dado
 export async function deleteSales(req) {
   let id = req.params.id
-  salesinho.get('DELETE FROM Sales2 WHERE id=?', [id])
+  db.get('DELETE FROM Sales WHERE id=?', [id])
   return {
-    id: id,  "message": "Delete accepted huh, no return! "
+    id: id, "message": "Delete accepted huh, no return! "
   };
 };
